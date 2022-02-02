ea7e9643d2c112373b7e0fdbc1f00c844c2f650c
Lorena Silva <lorena.silva@codeminer42.com>
2022-02-02 17:42:16 -0300

    add folder coverage in gitignore file, and fixed indentation

--- app.rb
+++ app.rb
@@ -2,20 +2,14 @@ require "sinatra"
 require "sinatra/reloader" if development?
   
   get '/' do
-
     erb :index
-
   end
   
   get '/link' do
-    
     erb :link
-
   end
 
   get '/academic' do
-
     erb :academic
-
   end
   
